04f59dc91228a7e1cd9ff67638df8f0b5732aacb
mitchneal <65240017+mitchneal@users.noreply.github.com>
2025-04-08 19:21:51 +0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -13,7 +13,7 @@ EOF
   mount "$1"
   # mount -v -t cifs "$1" "$2" -o credentials="$3"
 }
-  AddShare "//192.168.3.24/mnt/mFS1/-MountPoint/CT159-vscode" "/home/SetupArch" "/root/smb-credentials-WU1.cifs" okminh "$1"
+  AddShare "//192.168.3.24/mnt/mFS1/-MountPoint/CT159-vscode" "/home/SetupArch" "/root/smb-credentials-WU1.cifs" okminh "Samba21751@"
 
 cd "/home/SetupArch/BeProArch"
 bash setup.sh
